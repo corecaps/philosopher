6b3712f33f9722e2641c111faa61bac9e82fe197
corecaps <corecaps@gmail.com>
2023-01-30 13:14:53 +0100

    basic Makefile & usage exit

--- philo/main.c
+++ philo/main.c
@@ -12,3 +12,19 @@
 
 #include "philo.h"
 
+static void	usage(void)
+{
+	printf("Usage: ./philo number_of_philosophers time_to_die time_to_eat");
+	printf(" time_to_sleep [number_of_times_each_philosopher_must_eat]\n");
+	printf("\ttimes are in milliseconds\n");
+	printf("\tnumber_of_philosophers must be greater than 1\n");
+	exit(1);
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argv;
+	if (argc != 5 && argc != 6)
+		usage();
+	return (0);
+}
--- philo/philo.h
+++ philo/philo.h
@@ -10,4 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHILO_H
+# define PHILO_H
+# include <pthread.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <stdio.h>
 
+#endif
